6bcd7e5def11f216ae473b933f64aa86174385b8
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-02-25 20:10:55 +0530

    Added : Ability To Create Adding 30 & 56 to 70 at last

--- src/com/bridgelabz/dsg/LinkedListMain.java
+++ src/com/bridgelabz/dsg/LinkedListMain.java
@@ -1,5 +1,5 @@
-/**UC2 : PROBLEM STATEMENT
- * Lets create a simple Linked List of 56, 30 and 70 Added first
+/**UC3 : PROBLEM STATEMENT
+ * Lets create a simple Linked List of 56, 30 and 70 Added last
  * @author Suraj Chaudhary
  * @date 25-Feb-2022
  */
@@ -12,9 +12,14 @@ public class LinkedListMain {
 		System.out.println("Welcome to linkedList DataStructure Program");
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter 1 to add the data at the start");
+		System.out.println("Enter 2 to add the data at the end");
 		switch (scanner.nextInt()) {
 		case 1:
 			Operations.addDataAtStart();
+			break;
+		case 2:
+			Operations.addDataAtEnd();
+			break;
 		}
 
 	}
--- src/com/bridgelabz/dsg/Operations.java
+++ src/com/bridgelabz/dsg/Operations.java
@@ -1,12 +1,20 @@
-/**
- * 
- */
+
 package com.bridgelabz.dsg;
 
-/**
- * @author Suraj Chaudhary
- * @date 25-Feb-2022
- */
 public class Operations {
+	public static void addDataAtStart() {
+		LinkedList linkedList = new LinkedList();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		linkedList.print();
+	}
 
+	public static void addDataAtEnd() {
+		LinkedList linkedList = new LinkedList();
+		linkedList.append(56);
+		linkedList.append(30);
+		linkedList.append(70);
+		linkedList.print();
+	}
 }
